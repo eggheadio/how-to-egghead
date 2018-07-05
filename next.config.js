33919d6a5211ea17f6fd726503f7f8fbd807aff2
johnlindquist <johnlindquist@gmail.com>
2018-07-05 11:50:58 -0600

    adding anchors

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
-const remarkHighlight = require('remark-highlight.js')
+const remarkHighlight = require("remark-highlight.js")
+const remarkSlug = require("remark-slug")
+const remarkAutolinkHeadings = require("remark-autolink-headings")
 
 module.exports = {
-  pageExtensions: ['js', 'md'],
+  pageExtensions: ["js", "md"],
   webpack: (config, { defaultLoaders }) => {
     config.module.rules.push({
       test: /\.md$/,
       use: [
         defaultLoaders.babel,
         {
-          loader: '@mdx-js/loader',
+          loader: "@mdx-js/loader",
           options: {
-            mdPlugins: [remarkHighlight]
+            behaviour: "wrap",
+            mdPlugins: [remarkHighlight, remarkSlug, remarkAutolinkHeadings]
           }
         }
       ]
