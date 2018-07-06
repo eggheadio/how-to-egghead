2af57aae6aa40090778590a8a24aff249fb66dd6
Creeland A. Provinsal <cree@egghead.io>
2018-07-06 14:31:58 -0700

    add chapter objects to section arrays

--- instructor-guide/docs/02-Creating-lessons/index.js
+++ instructor-guide/docs/02-Creating-lessons/index.js
@@ -1,9 +1,63 @@
 import WhatMakesAGreatLesson from "./01-What-makes-a-great-lesson.md"
+import WhatShouldYouTeach from './02-What-should-you-teach.md'
+import WhatIfALessonAlreadyExists from './03-What-if-a-lesson-already-exists.md'
+import WriteTheTitleAndSummary from './04-Write-the-title-and-summary.md'
+import CreateYourCodeExample from './05-Create-your-code-example.md'
+import HowToInstruct from './06-How-to-instruct.md'
+import SpeakConcisely from './07-Speak-concisely.md'
+import TheShowYourWorkTrick from './08-The-show-your-work-trick.md'
+import RecordAndUploadYourLesson from './09-Record-and-upload-your-lesson.md'
+import WhenWellPublishYourLesson from './10-When-well-publish-your-lesson.md'
 
 export default [
   {
     component: WhatMakesAGreatLesson,
     title: "What Makes a Great Lesson",
     slug: "what-makes-a-great-lesson"
+  },
+  {
+    component: WhatShouldYouTeach,
+    title: "What Should You Teach",
+    slug: "what-makes-a-great-lesson"
+  },
+  {
+    component: WhatIfALessonAlreadyExists,
+    title: "What if a Lesson Already Exists",
+    slug: "what-if-a-lesson-already-exists",
+  },
+  {
+    component: WriteTheTitleAndSummary,
+    title: "Write the Title and Summary",
+    slug: "write-the-title-and-summary"
+  },
+  {
+    component: CreateYourCodeExample,
+    title: "Create Your Code Example",
+    slug: "create-your-code-example"
+  },
+  {
+    component: HowToInstruct,
+    title: "How to Instruct",
+    slug: "how-to-instruct"
+  },
+  {
+    component: SpeakConcisely,
+    title: "Speak Concisely",
+    slug: "speak-concisely"
+  },
+  {
+    component: TheShowYourWorkTrick,
+    title: "The Show Your Work Trick",
+    slug: "the-show-your-work-trick"
+  },
+  {
+    component: RecordAndUploadYourLesson,
+    title: "Record and Upload Your Lesson",
+    slug: "record-and-upload-your-lesson"
+  },
+  {
+    component: WhenWellPublishYourLesson,
+    title: "When We'll Publish Your Lesson",
+    slug: "when-well-publish-your-lesson"
   }
 ]
--- instructor-guide/docs/04-Screencasting-tips/index.js
+++ instructor-guide/docs/04-Screencasting-tips/index.js
@@ -1,9 +1,39 @@
 import PrepareYourScreenForRecording from "./01-Prepare-your-screen-for-recording.md"
+import SetUpYourAudio from "./02-Set-up-your-audio.md"
+import RecordYourLesson from "./03-Record-your-lesson.md"
+import EditYourLesson from "./04-Edit-your-lesson.md"
+import SharingYourCode from "./05-Sharing-your-code.md"
+import HostADemoRESTAPI from "./06-Host-a-demo-REST-API.md"
 
 export default [
   {
     component: PrepareYourScreenForRecording,
     title: "Prepare Your Screen for Recording",
     slug: "prepare-your-screen-for-recording"
+  },
+  {
+    component: SetUpYourAudio,
+    title: "Set Up Your Audio",
+    slug: "set-up-your-audio",
+  },
+  {
+    component: RecordYourLesson,
+    title: "Record Your Lesson",
+    slug: "record-your-lesson",
+  },
+  {
+    component: EditYourLesson,
+    title: "Edit Your Lesson",
+    slug: "edit-your-lesson",
+  },
+  {
+    component: SharingYourCode,
+    title: "Sharing Your Code",
+    slug: "sharing-your-code",
+  },
+  {
+    component: HostADemoRESTAPI,
+    title: "Host a Demo REST API",
+    slug: "host-a-demo-rest-api",
   }
 ]
