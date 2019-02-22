a4984ce68b7cc1438920ce078203702f68b7fbca
Joel Hooks <joel@egghead.io>
2019-02-22 13:31:27 -0800

    starts building a different structure

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,38 @@
-const { createFilePath } = require('gatsby-source-filesystem')
+const {createFilePath} = require('gatsby-source-filesystem')
+const _ = require('lodash')
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+exports.onCreateNode = ({node, actions, getNode}) => {
+  const {createNodeField} = actions
 
   if (node.internal.type === 'Mdx') {
-    const value = createFilePath({ node, getNode })
-
+    const value = createFilePath({node, getNode})
     createNodeField({
       name: 'slug',
       node,
-      value: `${value}`,
+      value: _.get(node, 'internal.frontmatter.slug', value),
     })
   }
 }
 const path = require('path')
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+exports.createPages = ({graphql, actions}) => {
+  const {createPage} = actions
   return new Promise((resolve, reject) => {
     resolve(
       graphql(
         `
           {
-            allMdx(sort: { order: ASC, fields: fields___slug }) {
+            allMdx(sort: {order: ASC, fields: fields___slug}) {
               edges {
                 node {
                   id
+                  parent {
+                    ... on File {
+                      name
+                      sourceInstanceName
+                    }
+                  }
+                  excerpt(pruneLength: 250)
                   fields {
                     slug
                   }
@@ -37,26 +44,39 @@ exports.createPages = ({ graphql, actions }) => {
               }
             }
           }
-        `
+        `,
       ).then(result => {
         if (result.errors) {
           console.error(result.errors)
           reject(result.errors)
         }
 
-        const guides = result.data.allMdx.edges
+        const guides = result.data.allMdx.edges.filter(e => {
+          return e.node.parent.sourceInstanceName !== 'articles'
+        })
 
-        guides.forEach(({ node }, index) => {
-          const previous =
-            index === guides.length - 1 ? null : guides[index + 1].node
+        const articles = result.data.allMdx.edges.filter(e => {
+          return e.node.parent.sourceInstanceName === 'articles'
+        })
+
+        articles.forEach(({node}) => {
+          createPage({
+            path: node.fields.slug,
+            component: path.resolve(`./src/templates/article.js`),
+            context: {id: node.id},
+          })
+        })
+
+        guides.forEach(({node}, index) => {
+          const previous = index === guides.length - 1 ? null : guides[index + 1].node
           const next = index === 0 ? null : guides[index - 1].node
           createPage({
             path: node.frontmatter.slug,
             component: path.resolve(`./src/templates/chapter.js`),
-            context: { id: node.id, previous, next },
+            context: {id: node.id, previous, next},
           })
         })
-      })
+      }),
     )
   })
 }
---
+++ src/templates/article.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {graphql} from 'gatsby'
+import {css} from '@emotion/core'
+import MDXRenderer from 'gatsby-mdx/mdx-renderer'
+import ArticleLayout from '../components/article-layout'
+import {bpMinLG} from '../utils/breakpoints'
+
+class ArticleTemplate extends React.Component {
+  render() {
+    const article = this.props.data.mdx
+    return (
+      <ArticleLayout>
+        <h1
+          css={css`
+            font-size: 15px;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+            opacity: 0.7;
+            ${bpMinLG} {
+              ${article.frontmatter.title && 'margin-top: -10px;'}
+            }
+            margin-top: 20px;
+          `}
+        >
+          {article.frontmatter.title && article.frontmatter.title}
+        </h1>
+        <MDXRenderer>{article.code.body}</MDXRenderer>
+      </ArticleLayout>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query ArticleQuery($id: String) {
+    mdx(id: {eq: $id}) {
+      id
+      frontmatter {
+        title
+        slug
+      }
+      code {
+        body
+      }
+    }
+  }
+`
+
+export default ArticleTemplate
