0ec02800a96005dd6f17ac2f20abf3bc7a5fa706
Joel Hooks <joelhooks@gmail.com>
2019-08-29 17:29:29 -0700

    more sorting

---
+++ gatsby-browser.js
@@ -0,0 +1,12 @@
+/* global window, document */
+const scrollTo = id => () => {
+  const el = document.querySelector(id)
+  if (el) return window.scrollTo(0, el.offsetTop - 20)
+  return false
+}
+
+export const onRouteUpdate = ({location: {hash}}) => {
+  if (hash) {
+    window.setTimeout(scrollTo(hash), 10)
+  }
+}
