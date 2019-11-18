5c95517a5442de5cbf8efd1ca470610c5caa6904
Joel Hooks <joelhooks@gmail.com>
2019-11-18 13:20:58 -0800

    turn off annoying auto complete

--- src/components/search.js
+++ src/components/search.js
@@ -1,71 +1,75 @@
-import React, {useState} from 'react'
-import {css} from '@emotion/core'
-import styled from '@emotion/styled'
-import Link from './link'
-import SearchIcon from '../images/icons/magnifier.svg'
-import SearchIconActive from '../images/icons/magnifier-active.svg'
-import algoliasearch from 'algoliasearch/lite'
-import {bpMaxSM} from '../utils/breakpoints'
+import React, { useState } from "react";
+import { css } from "@emotion/core";
+import styled from "@emotion/styled";
+import Link from "./link";
+import SearchIcon from "../images/icons/magnifier.svg";
+import SearchIconActive from "../images/icons/magnifier-active.svg";
+import algoliasearch from "algoliasearch/lite";
+import { bpMaxSM } from "../utils/breakpoints";
 import {
   InstantSearch,
   Configure,
   Highlight,
   connectHits,
   connectSearchBox,
-  connectStateResults,
-} from 'react-instantsearch-dom'
+  connectStateResults
+} from "react-instantsearch-dom";
 
-const client = algoliasearch('NLOD4N9T1X', '27ac39ffd2f1ec6477d7772f42ec3bb0')
+const client = algoliasearch("NLOD4N9T1X", "27ac39ffd2f1ec6477d7772f42ec3bb0");
 
-const Hits = connectHits(({hits}) => (
+const Hits = connectHits(({ hits }) => (
   <div
     css={css({
-      width: '100%',
-      position: 'absolute',
+      width: "100%",
+      position: "absolute",
       zIndex: 999,
-      backgroundColor: 'white',
-      overflow: 'auto',
+      backgroundColor: "white",
+      overflow: "auto",
       borderRadius: 3,
       marginTop: 6,
       maxHeight: 500,
-      border: '1px solid rgba(0,0,0,0.08)',
+      border: "1px solid rgba(0,0,0,0.08)",
       boxShadow:
-        '0 3px 6px 0 rgba(0,0,0,0.12), 0 7px 24px 0 rgba(62,61,68,0.12)',
-    })}>
-    <ul css={{margin: 0, padding: '10px 0'}}>
+        "0 3px 6px 0 rgba(0,0,0,0.12), 0 7px 24px 0 rgba(62,61,68,0.12)"
+    })}
+  >
+    <ul css={{ margin: 0, padding: "10px 0" }}>
       {hits.map(hit => (
         <li
           key={hit.objectID}
           css={css({
-            display: 'flex',
-            width: '100%',
-            padding: '2px 16px',
-            marginBottom: 5,
-          })}>
+            display: "flex",
+            width: "100%",
+            padding: "2px 16px",
+            marginBottom: 5
+          })}
+        >
           <div
             css={css({
-              width: '100%',
-            })}>
+              width: "100%"
+            })}
+          >
             <Link
               to={hit.slug}
               id={hit.slug}
               css={css({
-                width: '100%',
-                height: '100%',
+                width: "100%",
+                height: "100%"
               })}
-              activeClassName="active">
+              activeClassName="active"
+            >
               <Highlight
                 attribute="title"
                 hit={hit}
                 tagName="mark"
                 css={{
                   fontSize: 18,
-                  [bpMaxSM]: {fontSize: 16, lineHeight: '16px'},
-                  lineHeight: '18px',
+                  [bpMaxSM]: { fontSize: 16, lineHeight: "16px" },
+                  lineHeight: "18px"
                 }}
               />
               <Highlight
-                css={css({display: 'block', fontSize: 14, opacity: 0.8})}
+                css={css({ display: "block", fontSize: 14, opacity: 0.8 })}
                 attribute="text"
                 hit={hit}
                 tagName="mark"
@@ -94,82 +98,86 @@ const Hits = connectHits(({hits}) => (
       ))}
     </ul>
   </div>
-))
+));
 
-const Label = styled('label')`
+const Label = styled("label")`
   display: block;
-`
+`;
 
-const Input = styled('input')({
-  fontFamily: 'system-ui, sans-serif',
-  fontWeight: 'normal',
-  borderRadius: '5px',
+const Input = styled("input")({
+  fontFamily: "system-ui, sans-serif",
+  fontWeight: "normal",
+  borderRadius: "5px",
   background: `url(${SearchIcon}) no-repeat`,
-  backgroundSize: '16px',
-  backgroundPositionX: '12px',
-  backgroundPositionY: '50%',
-  ':focus': {
+  backgroundSize: "16px",
+  backgroundPositionX: "12px",
+  backgroundPositionY: "50%",
+  ":focus": {
     background: `url(${SearchIconActive}) no-repeat`,
-    backgroundSize: '16px',
-    backgroundPositionX: '12px',
-    backgroundPositionY: '50%',
-    outlineColor: '#FF90A4',
+    backgroundSize: "16px",
+    backgroundPositionX: "12px",
+    backgroundPositionY: "50%",
+    outlineColor: "#FF90A4"
   },
-  backgroundColor: '#fafafa',
-  border: '1px solid #f1f1f1',
-  display: 'block',
-  fontSize: '16px',
-  padding: '0.5rem 0.75rem 0.5rem 35px',
-  width: '100%',
-  boxShadow: 'none',
-})
+  backgroundColor: "#fafafa",
+  border: "1px solid #f1f1f1",
+  display: "block",
+  fontSize: "16px",
+  padding: "0.5rem 0.75rem 0.5rem 35px",
+  width: "100%",
+  boxShadow: "none"
+});
 
-const Search = connectSearchBox(({currentRefinement, refine, setIsActive}) => (
-  <form noValidate action="" role="search" css={css({margin: 0})}>
-    <Label htmlFor="search">
-      <Input
-        placeholder="Search the guide"
-        type="search"
-        id="search"
-        value={currentRefinement}
-        onBlur={() => {
-          if (currentRefinement === '') {
-            setIsActive(false)
-          }
-        }}
-        onChange={event => {
-          setIsActive(true)
-          refine(event.currentTarget.value)
-        }}
-      />
-    </Label>
-  </form>
-))
+const Search = connectSearchBox(
+  ({ currentRefinement, refine, setIsActive }) => (
+    <form noValidate action="" role="search" css={css({ margin: 0 })}>
+      <Label htmlFor="search">
+        <Input
+          placeholder="Search the guide"
+          type="search"
+          autocomplete="off"
+          id="search"
+          value={currentRefinement}
+          onBlur={() => {
+            if (currentRefinement === "") {
+              setIsActive(false);
+            }
+          }}
+          onChange={event => {
+            setIsActive(true);
+            refine(event.currentTarget.value);
+          }}
+        />
+      </Label>
+    </form>
+  )
+);
 
-const SearchContainer = styled('div')`
+const SearchContainer = styled("div")`
   width: 100%;
-`
+`;
 
 // Show search results after user starts typing
-const Results = connectStateResults(({searchState, searchResults}) =>
+const Results = connectStateResults(({ searchState, searchResults }) =>
   searchState && searchState.query ? (
-    <div css={{position: 'relative'}}>
+    <div css={{ position: "relative" }}>
       <Hits />
     </div>
   ) : null
-)
+);
 export default () => {
-  const [isActive, setIsActive] = useState(false)
+  const [isActive, setIsActive] = useState(false);
   return (
     <InstantSearch
       searchClient={client}
       indexName={process.env.ALGOLIA_INDEX_NAME}
-      root={{Root: SearchContainer}}>
+      root={{ Root: SearchContainer }}
+    >
       <Configure distinct={1} hitsPerPage={30} />
       <div>
         <Search setIsActive={setIsActive} />
         <Results />
       </div>
     </InstantSearch>
-  )
-}
+  );
+};
