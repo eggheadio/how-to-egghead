96add7cc00f731b70c5db8e384fac77f9ddd7d53
Lauro Silva <57044804+laurosilvacom@users.noreply.github.com>
2020-04-02 07:20:59 -0700

    Update 'Setup your Mac terminal for screencasting' article with new lesson embed (#127)
    * Adding egghead video embed component
    
    * Adding lesson summarized
    
    Lesson: Customize the Command Prompt in VS Code
    
    * Adding style to pre and code tags
    
    Adding a gray background to pre and code tags, following the default colors.
    
    * Editing code styles + lesson code snippet

---
+++ src/components/eggheadEmbed.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const EggheadEmbed = ({ lessonLink, lessonTitle }) => (
+  <div
+    style={{
+      position: 'relative',
+      paddingBottom: '56.2%',
+      marginBottom: '40px',
+    }}
+  >
+    <iframe
+      width="100%"
+      height="100%"
+      src={lessonLink}
+      title={lessonTitle}
+      frameBorder="0"
+      allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreenstyle="position: absolute;width: 100%;height: 100%;"
+      style={{
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+      }}
+    />
+  </div>
+);
+
+export default EggheadEmbed;
